4be09ed095ddab20da2c3b2b393e7159003d3512
Naithanial Loves <naitlp@outlook.com>
2022-01-26 21:46:01 +1100

    Add Color App to Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import TodoList from './TodoList';
 
-function App()
-{
- return
- (
+function App(){
+ return (
     <TodoList />
  )  
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function TodoList() 
 {
-  return 
+  return (
   <div>
       Hello World
-  </div>;
+  </div>
+  )
 }
